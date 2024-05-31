54242155bed2ef3897bf431578f8701fba8d087d
Marcoslevi <65694101+Marcoslevit007@users.noreply.github.com>
2024-05-30 22:24:12 -0300

    Adicionando operadores aritimeticos

---
+++ dados.php
@@ -0,0 +1,19 @@
+<?php
+
+//Operadores aritimeticos
+
+$num1 = 10;
+$num2 = 5;
+
+//Soma
+$resultado = $num1 + $num2;
+//Subtração
+$resultado = $num1 - $num2;
+//Divisão
+$resultado = $num1 / $num2;
+//Multiplicação
+$resultado = $num1 * $num2;
+//Potencia
+$resultado = $num1 ** $num2;
+//Resto
+$resultado = $num1 % $num2;
